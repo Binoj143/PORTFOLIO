aa8785151692ef964d0c14a1191352c98d04ea62
Binoj B Chandran <binojbc3315@gmail.com>
2025-11-17 16:01:50 +0400

    Rebuild: Trigger clean build on Netlify

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Work from './components/Work';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
+// Main app component
 function App() {
   return (
     <>
